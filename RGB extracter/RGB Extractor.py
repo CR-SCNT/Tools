76c744cf4d494b63879a8571181847b5cef6226b
CR-SCNT <nmksva@gmail.com>
2024-07-03 22:15:48 +0800

    Update RGB Extractor.py

--- RGB extracter/RGB Extractor.py
+++ RGB extracter/RGB Extractor.py
@@ -1,11 +1,12 @@
 import sys
 from PIL import Image
 
-batch = sys.argv[1]
 
 
-if batch != 0 or batch != 1:
-    raise("Wrong argument")
+batch = int(sys.argv[1])
+
+if batch != 0 and batch != 1:
+    raise(KeyError("Wrong argument"))
 
 if batch == 0:
     image = Image.open("pic.jpg")
@@ -14,14 +15,14 @@ if batch == 0:
     R, G, B = pixels[width/2, height/2]
     print("pic.jpg -> (", R, " ", G, " ", B, ")"  )
 elif batch == 1:
-    if sys.argv.length() <= 2:
+    if len(sys.argv) <= 2:
         number = int(input("Please enter the number of files you want process (from 1 to 8)"))
     else:
         number = sys.argv[2]
     temps = ["1", "2", "3", "4", "5", "6", "7", "8"]
     for i in range(number):
         filename = "pic" + temps[i] + ".jpg"
-        image = Image.opne(filename)
+        image = Image.open(filename)
         pixels = image.load()
         width, height = image.size
         R, G, B = pixels[width/2, height/2]
